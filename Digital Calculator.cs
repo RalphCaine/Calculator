0e4e3dcc81ac6975d79b21406933116575476087
RalphCaine <164994664+RalphCaine@users.noreply.github.com>
2024-03-26 11:16:18 +0400

    Add files via upload

---
+++ Digital Calculator.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Digital_Calculator_Comp
+{
+    public partial class Calculator : Form
+    {
+        private decimal valueFirst = 0.0m;
+        private decimal valueSecond = 0.0m;
+        private decimal Result = 0.0m;
+        private string operators = "+";
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            valueFirst = decimal.Parse(TxtBox.Text);
+            TxtBox.Clear();
+            operators = "-";
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            valueFirst = decimal.Parse(TxtBox.Text);
+            TxtBox.Clear();
+            operators = "*";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            if (TxtBox.Text.Contains("-"))
+            {
+                TxtBox.Text = TxtBox.Text.Trim('-');
+            }
+            else
+            {
+                TxtBox.Text = "-" + TxtBox.Text;
+            }
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            valueFirst = decimal.Parse(TxtBox.Text);
+            TxtBox.Clear();
+            operators = "%";
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            valueFirst = decimal.Parse(TxtBox.Text);
+            TxtBox.Clear();
+            operators = "+";
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            valueFirst = 0.0m;
+            valueSecond = 0.0m;
+            TxtBox.Text = "0";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            valueFirst = decimal.Parse(TxtBox.Text);
+            TxtBox.Clear();
+            operators = "/";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+             switch (operators) {
+                case "-":
+                    valueSecond = decimal.Parse(TxtBox.Text);
+                    Result = valueFirst - valueSecond;
+                    TxtBox.Text = Result.ToString();
+                    break;
+                case "+":
+                    valueSecond = decimal.Parse(TxtBox.Text);
+                    Result = valueFirst + valueSecond;
+                    TxtBox.Text = Result.ToString();
+                    break;
+                case "*":
+                    valueSecond = decimal.Parse(TxtBox.Text);
+                    Result = valueFirst * valueSecond;
+                    TxtBox.Text = Result.ToString();
+                    break;
+                case "/":
+                    valueSecond = decimal.Parse(TxtBox.Text);
+                    Result = valueFirst / valueSecond;
+                    TxtBox.Text = Result.ToString();
+                    break;
+                case "%":
+                    valueSecond = decimal.Parse(TxtBox.Text);
+                    Result = valueFirst % valueSecond;
+                    TxtBox.Text = Result.ToString();
+                    break;
+
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (TxtBox.Text == "0")
+            {
+                TxtBox.Text = "3";
+            }
+            else
+            {
+                TxtBox.Text += "3";
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (TxtBox.Text == "0")
+            {
+                TxtBox.Text = "6";
+            }
+            else
+            {
+                TxtBox.Text += "6";
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (TxtBox.Text == "0")
+            {
+                TxtBox.Text = "9";
+            }
+            else
+            {
+                TxtBox.Text += "9";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (!TxtBox.Text.Contains(".")) {
+                TxtBox.Text += ".";
+            }
+                
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (TxtBox.Text == "0")
+            {
+                TxtBox.Text = "2";
+            }
+            else
+            {
+                TxtBox.Text += "2";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (TxtBox.Text == "0")
+            {
+                TxtBox.Text = "5";
+            }
+            else
+            {
+                TxtBox.Text += "5";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (TxtBox.Text == "0")
+            {
+                TxtBox.Text = "8";
+            }
+            else
+            {
+                TxtBox.Text += "8";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (TxtBox.Text == "0")
+            {
+                TxtBox.Text = "0";
+            }
+            else
+            {
+                TxtBox.Text += "0";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (TxtBox.Text == "0")
+            {
+                TxtBox.Text = "1";
+            }
+            else
+            {
+                TxtBox.Text += "1";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (TxtBox.Text == "0")
+            {
+                TxtBox.Text = "4";
+            }
+            else
+            {
+                TxtBox.Text += "4";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (TxtBox.Text == "0")
+            {
+                TxtBox.Text = "7";
+            }
+            else
+            {
+                TxtBox.Text += "7";
+            }
+        }
+
+        private void TxtBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
